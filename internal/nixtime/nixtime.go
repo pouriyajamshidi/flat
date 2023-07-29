fa6e9d948727656c134aaa83fe65167742ede0dc
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2023-07-29 10:27:44 +0200

    add kernel boot time in nanoseconds support

---
+++ internal/nixtime/nixtime.go
@@ -0,0 +1,18 @@
+package nixtime
+
+// #cgo CFLAGS: -g -Wall
+/*
+#include <time.h>
+
+static unsigned long long get_nsecs(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    return (unsigned long long)ts.tv_sec * 1000000000UL + ts.tv_nsec;
+}
+*/
+import "C"
+
+func GetNanosecSinceBoot() uint64 {
+	return uint64(C.get_nsecs())
+}
