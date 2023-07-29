c4a5faf7cf278ddd1b9111d39f6d8f77a1178707
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2023-07-29 10:31:24 +0200

    add clsact support

--- clsact/clsact.go
+++ clsact/clsact.go
@@ -1,1 +1,19 @@
 package clsact
+
+import "github.com/vishvananda/netlink"
+
+type ClsAct struct {
+	attrs *netlink.QdiscAttrs
+}
+
+func NewClsAct(attrs *netlink.QdiscAttrs) *ClsAct {
+	return &ClsAct{attrs: attrs}
+}
+
+func (qdisc *ClsAct) Attrs() *netlink.QdiscAttrs {
+	return qdisc.attrs
+}
+
+func (qdisc *ClsAct) Type() string {
+	return "clsact"
+}
