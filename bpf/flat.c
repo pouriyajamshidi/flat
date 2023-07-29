240fc480e3215b93aebc5986716fc91524c6ad76
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2023-07-29 10:30:24 +0200

    add tcp and udp support

---
+++ bpf/flat.c
@@ -0,0 +1,166 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <linux/bpf.h>
+#include <linux/bpf_common.h>
+
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/in.h>
+#include <linux/in6.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/pkt_cls.h>
+
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} pipe SEC(".maps");
+
+struct packet_t {
+    struct in6_addr src_ip;
+    struct in6_addr dst_ip;
+    __be16 src_port;
+    __be16 dst_port;
+    __u8 protocol;
+    bool syn;
+    bool ack;
+    uint64_t ts;
+};
+
+static inline int handle_ip_packet(uint8_t* head, uint8_t* tail, uint32_t* offset, struct packet_t* pkt) {
+    struct ethhdr* eth = (void*)head;
+    struct iphdr* ip;
+    struct ipv6hdr* ipv6;
+
+    switch (bpf_ntohs(eth->h_proto)) {
+    case ETH_P_IP:
+        *offset = sizeof(struct ethhdr) + sizeof(struct iphdr);
+
+        if (head + (*offset) > tail) { // If the next layer is not IP, let the packet pass
+            return TC_ACT_OK;
+        }
+
+        ip = (void*)head + sizeof(struct ethhdr);
+
+        if (ip->protocol != IPPROTO_TCP && ip->protocol != IPPROTO_UDP) {
+            return TC_ACT_OK;
+        }
+
+        // Create IPv4-Mapped IPv6 Address
+        pkt->src_ip.in6_u.u6_addr32[3] = ip->saddr;
+        pkt->dst_ip.in6_u.u6_addr32[3] = ip->daddr;
+
+        // Pad the field before IP address with all Fs just like the RFC
+        pkt->src_ip.in6_u.u6_addr16[5] = 0xffff;
+        pkt->dst_ip.in6_u.u6_addr16[5] = 0xffff;
+
+        pkt->protocol = ip->protocol;
+
+        return 1; // We have a TCP or UDP packet!
+
+    case ETH_P_IPV6:
+        *offset = sizeof(struct ethhdr) + sizeof(struct ipv6hdr);
+
+        if (head + (*offset) > tail) {
+            return TC_ACT_OK;
+        }
+
+        ipv6 = (void*)head + sizeof(struct ethhdr);
+
+        if (ipv6->nexthdr != IPPROTO_TCP && ipv6->nexthdr != IPPROTO_UDP) {
+            return TC_ACT_OK;
+        }
+
+        pkt->src_ip = ipv6->saddr;
+        pkt->dst_ip = ipv6->daddr;
+
+        pkt->protocol = ipv6->nexthdr;
+
+        return 1; // We have a TCP or UDP packet!
+
+    default:
+        return TC_ACT_OK;
+    }
+}
+
+static inline int handle_ip_segment(uint8_t* head, uint8_t* tail, uint32_t* offset, struct packet_t* pkt) {
+    struct tcphdr* tcp;
+    struct udphdr* udp;
+
+    switch (pkt->protocol) {
+    case IPPROTO_TCP:
+        tcp = (void*)head + *offset;
+
+        if (tcp->syn) { // We have SYN or SYN/ACK
+            pkt->src_port = tcp->source;
+            pkt->dst_port = tcp->dest;
+            pkt->syn = tcp->syn;
+            pkt->ack = tcp->ack;
+            pkt->ts = bpf_ktime_get_ns();
+
+            return 1;
+        }
+    case IPPROTO_UDP:
+        udp = (void*)head + *offset;
+
+        pkt->src_port = udp->source;
+        pkt->dst_port = udp->dest;
+        pkt->ts = bpf_ktime_get_ns();
+
+        return 1;
+
+    default:
+        return TC_ACT_OK;
+    }
+}
+
+SEC("tc")
+int flat(struct __sk_buff* skb) {
+
+    if (bpf_skb_pull_data(skb, 0) < 0) {
+        return TC_ACT_OK;
+    }
+
+    // We only want unicast packets
+    if (skb->pkt_type == PACKET_BROADCAST || skb->pkt_type == PACKET_MULTICAST) {
+        return TC_ACT_OK;
+    }
+
+    uint8_t* head = (uint8_t*)(long)skb->data;     // Start of the packet data
+    uint8_t* tail = (uint8_t*)(long)skb->data_end; // End of the packet data
+
+    if (head + sizeof(struct ethhdr) > tail) { // Not an Ethernet frame
+        return TC_ACT_OK;
+    }
+
+    struct packet_t pkt = { 0 };
+
+    uint32_t offset = 0;
+
+    if (handle_ip_packet(head, tail, &offset, &pkt) == TC_ACT_OK) {
+        return TC_ACT_OK;
+    }
+
+    // Cehck if TCP/UDP header is not fitting this packet
+    if (head + offset + sizeof(struct tcphdr) > tail || head + offset + sizeof(struct udphdr) > tail) {
+        return TC_ACT_OK;
+    }
+
+    if (handle_ip_segment(head, tail, &offset, &pkt) == TC_ACT_OK) {
+        return TC_ACT_OK;
+    }
+
+    if (bpf_perf_event_output(skb, &pipe, BPF_F_CURRENT_CPU, &pkt, sizeof(pkt)) < 0) {
+        return TC_ACT_OK;
+    }
+
+    return TC_ACT_OK;
+}
+
+char _license[] SEC("license") = "Dual MIT/GPL";
